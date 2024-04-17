b84b813a3dede2d016bc5dc0770dbf14617ebda6
George <georgeey123@gmail.com>
2024-04-17 16:54:12 +0000

    refactored project structure

--- src/CleanArchBlog_crud.Core/Interfaces/IEmailSender.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CleanArchBlog_crud.Core.Interfaces;
-
-public interface IEmailSender
-{
-  Task SendEmailAsync(string to, string from, string subject, string body);
-}
--- src/CleanArchBlog_crud.Domain/obj/Debug/net8.0/CleanArchBlog_crud.Domain.AssemblyInfo.cs
+++ src/CleanArchBlog_crud.Domain/obj/Debug/net8.0/CleanArchBlog_crud.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchBlog_crud.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a95c0505fc7cd5410b4d5cd429f419dac17e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchBlog_crud.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchBlog_crud.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
